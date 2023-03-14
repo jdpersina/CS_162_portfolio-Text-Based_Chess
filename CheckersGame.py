1b136eed4a73410b4efee943ce686c20177b22e5
danpersina <jdpersina@gmail.com>
2023-03-14 15:24:03 -0400

    good progress2

--- CheckersGame.py
+++ CheckersGame.py
@@ -26,7 +26,19 @@ class Piece:
         self._condition = 0
 
     def return_condition(self):
+        """
+        This method shows whether the piece is normal, king, or a triple king
+        """
         return self._condition
+
+    def return_location(self):
+        """
+        This method shows the piece's location on the board
+        """
+        return self._location
+
+    def show_info(self):
+
 
 class Checkers:
     """
@@ -48,8 +60,6 @@ class Checkers:
                 [(1, 2), (2, 2), (3, 2), (4, 2), (5, 2), (6, 2), (7, 2), (8, 2)],
                 [(1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1), (8, 1)]
             ]
-
-        # working on board setting, probably won't go here, maybe goes in create player method
 
     def append_player_list(self, Player: object):
         self._players.append(Player)
